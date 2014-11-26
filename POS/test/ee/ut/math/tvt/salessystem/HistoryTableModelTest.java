a2b25c998e0b7d3009f9639c3e3844ffde3e28c2
Joosep <joosepnorma@gmail.com>
2014-11-26 19:34:38 +0200

    historyTableModelTest

---
+++ POS/test/ee/ut/math/tvt/salessystem/HistoryTableModelTest.java
@@ -0,0 +1,26 @@
+package ee.ut.math.tvt.salessystem;
+
+import org.junit.Before;
+
+import ee.ut.math.tvt.salessystem.domain.data.HistoryItem;
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.HistoryTableModel;
+
+public class HistoryTableModelTest {
+	
+	private HistoryTableModel historyTable;
+	private HistoryItem historyItem;
+	
+	@Before
+	public void setUp() {
+		historyItem = new HistoryItem(1, "2014-10-11", "19:30", 2.50);
+		historyTable = new HistoryTableModel();
+	  }
+	
+	// TODO
+    public void testSomethingWithHistoryTable(){
+    	
+    }
+
+}
